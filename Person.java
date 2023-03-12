5ec4051cc5bc0bcfd628d429a8ecd4cf4bd5b998
Extranjero13 <arivera0229@yahoo.com>
2023-03-11 23:25:40 -0500

    Test 2

--- Person.java
+++ Person.java
@@ -1,12 +1,36 @@
-import business.Company;
+
 
 public class Person {
+
+  private String middleName = "Christopher";
+
   public void sayHello(){
     System.out.println("Hello");
+  }
+
+  public void saySomething(String something){
+    System.out.println(something);
+  }
+
+  public char getMiddleInitial(){
+    return middleName.charAt(0);
+  }
+
+  public int add(int x, int y){
+    return x+y;
   }
   public static void main(String[] args) {
     Person p1 = new Person();
     p1.sayHello();
+    p1.saySomething("My groovy message");
+    p1.saySomething("yoyoyoyoyo");
+
+    char middleInitial = p1.getMiddleInitial();  // alternative way to call this function  
+
+    System.out.println(p1.getMiddleInitial());
+    System.out.println(p1.add(5,3));
+    System.out.println(middleInitial);        // alternative way follow-up
+
   } 
     
 }

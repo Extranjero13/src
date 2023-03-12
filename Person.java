b23e6c731879846528644d25da5fd8780d684da1
Extranjero13 <arivera0229@yahoo.com>
2023-03-11 22:30:17 -0500

    Test

---
+++ Person.java
@@ -0,0 +1,12 @@
+import business.Company;
+
+public class Person {
+  public void sayHello(){
+    System.out.println("Hello");
+  }
+  public static void main(String[] args) {
+    Person p1 = new Person();
+    p1.sayHello();
+  } 
+    
+}
